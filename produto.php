d4e9899d8c73b4525e7345525dc17437e3947ca8
jucicleber <71895344+jucicleber@users.noreply.github.com>
2021-10-07 12:02:26 -0400

    ATUALIZAÇÃO

--- produto.php
+++ produto.php
@@ -3,9 +3,10 @@ include_once("connect.php");
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
+    <>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <style>
