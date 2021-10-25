4f605ea2076d3e96bfc49e8bad5c1ca6b306d9eb
jucicleber <71895344+jucicleber@users.noreply.github.com>
2021-10-25 11:05:56 -0400

    atualização

---
+++ criar_requisicao.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Criar requisição</title>
+</head>
+
+<body>
+    <h1>Para requisitar um produto preencha os itens abaixo</h1>
+</body>
+
+</html>
