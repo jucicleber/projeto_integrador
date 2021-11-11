bd67263aa7a843833e9680b5c222ec0de18c2c8d
jucicleber <71895344+jucicleber@users.noreply.github.com>
2021-11-11 12:23:30 -0400

    Update enviar_devolucao.php

--- enviar_devolucao.php
+++ enviar_devolucao.php
@@ -10,14 +10,10 @@ if($_POST){
 
     $query = "insert into devolucao(id_funcionario, id_produto, quantidade, id_requisicao) values('{$colaborador}',{$quantidade},'{$produto}',{$id_requisicao})";
     if ($result = $conn->query($query)) {
-        $id_requisicao = $conn->insert_id;
+        $id_devolucao = $conn->insert_id;
         header("Location:mostrar_devolucao.php?id_devolucao=$id_devolucao");
-        die();
     
     else{
         echo 'Os dados não conferem com nosso banco de dados!!!';
     }
-    }
-    else {
-        header("location: criar_devolucao.php");
-} 
+}
